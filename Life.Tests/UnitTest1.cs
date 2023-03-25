53f3464f154464b50e40f20ebf10e94c6b70eb87
Dobrynia100 <dobrynya2001@gmail.com>
2023-03-25 18:47:09 +0300

    Первый тест

--- Life.Tests/UnitTest1.cs
+++ Life.Tests/UnitTest1.cs
@@ -9,9 +9,19 @@ namespace NET
         [TestMethod]
         public void TestMethod1()
         {
-            
-            
-          
+            Board board= new Board(
+                width: 50,
+                height: 20,
+                cellSize: 1,
+                false,
+                liveDensity: 0.5);
+            board.dead();
+            bool check=true;
+            foreach (var cell in board.Cells)
+               if(cell.IsAlive)
+                   check = false;
+                    
+            Assert.IsTrue(check);
         }
         
     }
